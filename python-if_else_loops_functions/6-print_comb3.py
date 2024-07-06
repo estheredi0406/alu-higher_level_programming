ce0121125fedf66cd177ce13a0b08c20e1d5698b
estheredi0406 <e.sessenou@alustudent.com>
2024-07-06 00:43:09 -0700

    6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for x in range (0 ,10)
-   for j in range (x + 1, 10)
-     print(f"{i}{j}, ", end="")
-print(f"99\n")     
+for i in range(10):
+  for j in range(i + 1, 10):
+    print("{}{}".format(i, j), end=", ")
+print("99\n")
