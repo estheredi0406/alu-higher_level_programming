2cc1bc4df6810015d6382edf5bbdb5c26a151018
estheredi0406 <e.sessenou@alustudent.com>
2024-07-06 00:55:30 -0700

    done

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
   for j in range(i + 1, 10):
-    if j <= 9:  # Check if second digit is less than or equal to 9
-      print("{}{}".format(i, j), end=", ")
-  # Print 8 + second digit directly after the loop for i=7
-  if i == 7:
-    print("89")
-print()  # Separate newline for final output
+    print("{}{}".format(i, j), end=", ")
+print("89\n")
