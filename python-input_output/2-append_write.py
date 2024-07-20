0e97cc69b01372a1ece4784f860d711b9724d5d1
estheredi0406 <e.sessenou@alustudent.com>
2024-07-20 07:44:17 -0700

    done

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,19 @@
 """defining the function"""
 
 
-def write_file(filename="", text=""):
-    """reds filename"""
-    with open(filename, "a", encoding 'utf-8') as f:
-        return f.write(text)
+def append_write(filename="", text=""):
+    """
+    Appends a string to the end of a file, creating the file if it doesn't exist.
+
+    Parameters:
+        filename (str): The name of the file to append to.
+        text (str): The text to append to the file.
+
+    Returns:
+        int: The number of characters appended.
+    """
+    # Open the file in append mode ('a') with UTF-8 encoding
+    with open(filename, "a", encoding='utf-8') as file:
+        # Write the text to the file and return the number of characters written
+        return file.write(text)
+
