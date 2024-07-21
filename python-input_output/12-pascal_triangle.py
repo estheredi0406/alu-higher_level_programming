d1863bd72f27c8e6d63995d321da8918b2b3fe8d
estheredi0406 <e.sessenou@alustudent.com>
2024-07-21 05:02:22 -0700

    done

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
-"""defining function"""
+"""defining function
+
+"""
+    Generates Pascal's Triangle up to the nth row.
+
+    Parameters
+    ----------
+    n : int
+        The number of rows to generate in Pascal's Triangle. Must be a positive integer.
+
+    Returns
+    -------
+    list of list of int
+        A list representing Pascal's Triangle, where each element is a list of integers representing a row.
+
+    Raises
+    ------
+    ValueError
+        If n is less than or equal to 0.
+
+    Examples
+    --------
+    >>> pascal_triangle(5)
+    [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    """"""
 
 
 def pascal_function(n):
     """Represent Pascal's Triangle of size n.
     returns a list of lists of integers representing the triangle.
     """
+
     if n <= 0:
         return []
     
