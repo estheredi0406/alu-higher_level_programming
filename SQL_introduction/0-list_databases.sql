37e530c6e93b7a9f36e5d482a27d2dfc3ea534d8
estheredi0406 <e.sessenou@alustudent.com>
2024-07-21 05:49:57 -0700

    done

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,3 +1,15 @@
-#!/bin
-mysql -u username -p -e "SHOW DATABASES;"
+#!/bin/bash
+
+-- This script logs into MySQL and lists all databases
+
+-- Replace 'username' with your MySQL username
+USERNAME="root"
+
+-- Prompt for password
+echo "root"
+read -s PASSWORD
+
+-- Execute the MySQL command to show databases
+mysql -u $USERNAME -p$PASSWORD -e "SHOW DATABASES;"
+
 
