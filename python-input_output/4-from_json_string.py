1aa52b43162482a55d3a35bcd4abbc6dc7209e6e
estheredi0406 <e.sessenou@alustudent.com>
2024-07-21 02:25:22 -0700

    loads

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defining to_json_string"""
+
+
+import json
+
+
+def to_json_string(my_str):
+    """returns json representation of an object"""
+    return json.loads(my_str)
