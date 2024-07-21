06a58687c861bc468d9507e2929405b8b56bafc5
estheredi0406 <e.sessenou@alustudent.com>
2024-07-21 04:27:50 -0700

    done

---
+++ python-input_output/11-student.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Defines a Student class with attributes for first_name, last_name, and age.
+Includes methods to_json for serializing the instance into a dictionary, optionally filtering attributes,
+and reload_from_json for replacing all attributes of the Student instance based on a provided dictionary.
+"""
+
+class Student:
+    """
+    Represents a student with attributes for first_name, last_name, and age.
+    """
+    
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class.
+        
+        Parameters:
+        - first_name (str): The student's first name.
+        - last_name (str): The student's last name.
+        - age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age 
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance, optionally filtering attributes.
+
+        Parameters:
+        - attrs (list[str], optional): List of attribute names to include in the output. Defaults to None, meaning all attributes are included.
+
+        Returns:
+        - dict: A dictionary representing the student's information, filtered by the provided attribute names.
+        """
+        try:
+            for attr in attrs:
+                if type (attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict ()
+        for key, value in self.__dict__.items():
+            if key n attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        '''this function replaces all attributes of the student instance with the 
+        ones in the json argument '''
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
