1b735ffe45e52c0cbda7162c859bb0d7b35fb3c0
estheredi0406 <e.sessenou@alustudent.com>
2024-07-29 13:59:19 -0700

    user created

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,9 @@
 -- Lists all privileges of the users user_0d_1 and user_0d_2.
+
+
+select user ()
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
