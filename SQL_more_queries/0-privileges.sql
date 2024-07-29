eed24b39a1b729b073d6b104b6ae32d12d8ce3b9
estheredi0406 <e.sessenou@alustudent.com>
2024-07-29 14:12:23 -0700

    another one

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,4 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2
-
-
-SELECT USER ();
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
+-- Lists all privileges of the users 'user_0d_1' and 'user_0d_2'
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
