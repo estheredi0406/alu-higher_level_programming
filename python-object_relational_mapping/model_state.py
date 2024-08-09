4b596dbf55ddbe2319ee9c0c26a19256383018c6
estheredi0406 <e.sessenou@alustudent.com>
2024-08-09 05:47:02 -0700

    done

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-This module defines the State model, which is used to represent a state 
+This module defines the State model, which is used to represent a state
 in a MySQL database.
 
-The State model inherits from SQLAlchemy's Base and links to the MySQL 
+The State model inherits from SQLAlchemy's Base and links to the MySQL
 table 'states'. It includes the following attributes:
 - id: An integer that serves as the primary key.
 - name: A string representing the name of the state.
 
-The module uses SQLAlchemy's ORM to map the State class to the 'states' 
+The module uses SQLAlchemy's ORM to map the State class to the 'states'
 table in the database.
 """
 
@@ -28,4 +28,3 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
