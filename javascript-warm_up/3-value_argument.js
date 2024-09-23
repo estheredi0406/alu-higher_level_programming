58737ea6ab725c77b2b0a592ab8f25994fe4a6cf
estheredi0406 <e.sessenou@alustudent.com>
2024-09-23 03:03:59 -0700

    corrected

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 
 const firstArgument = args[0]
 
-if (args.length === 0) 
+if (firstArgument === undefined) 
 	{console.log('No argument');}
 	else {console.log(firstArgument);}
 	
