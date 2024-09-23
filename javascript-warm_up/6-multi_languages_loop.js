7600e22b6f885b4e865bd8dae9c47c533d1301a4
estheredi0406 <e.sessenou@alustudent.com>
2024-09-23 10:12:06 -0700

    done

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const messages = [
-   'C is fun',
-   'Python is cool',
-   'JavaScript is amazing',
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 for (let i = 0; i < messages.length; i++) {
-   console.log(messages[i]);
-} 
+  console.log(messages[i]);
+}
