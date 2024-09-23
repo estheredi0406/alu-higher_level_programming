c081d7375f646260764ed7492652a190bcde2c81
estheredi0406 <e.sessenou@alustudent.com>
2024-09-23 10:36:30 -0700

    done

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const size = parseInt(args[2], 10);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
