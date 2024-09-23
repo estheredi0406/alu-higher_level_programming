6b595343d5d05606c7edd3916d3ad4f3036195af
estheredi0406 <e.sessenou@alustudent.com>
2024-09-23 03:17:27 -0700

    synthhas corrected

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,9 +2,11 @@
 
 const args = process.argv.slice(2);
 
-const firstArgument = args[0]
+const firstArgument = args[0];
 
-if (firstArgument === undefined) 
-	{console.log('No argument');}
-	else {console.log(firstArgument);}
+if (firstArgument === undefined) {
+  console.log('No argument');
+} else {
+ console.log(firstArgument);
+}
 	
