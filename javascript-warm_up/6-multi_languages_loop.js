5e30b5e88e0c8151bcc0962d4d996636731be040
estheredi0406 <e.sessenou@alustudent.com>
2024-09-23 10:07:54 -0700

    synthax corrected

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const messages = [
+   'C is fun',
+   'Python is cool',
+   'Javascript is amazing',
+];
+
+for (let i = 0; i < messages.length; i++) {
+   console.log(messages[i]);
+} 
