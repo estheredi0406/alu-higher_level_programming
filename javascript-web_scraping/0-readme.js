633d4b8f84b5257e6a5266acb23e739eb3b6d58b
estheredi0406 <e.sessenou@alustudent.com>
2024-10-04 12:55:51 -0700

    line printed

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,19 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-fs.readFile(process.argv[2], function (err, data) {
-  err && console.log(err);
-  const content = data;
-  console.log(content.toString());
-	
+const filePath = process.argv[2];
+
+if (!filePath) {
+    console.log('Usage: node script.js <file_path>');
+    process.exit(1);
+}
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
